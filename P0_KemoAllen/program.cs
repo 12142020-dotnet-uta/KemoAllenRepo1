c0ec66f5f12a533b0eff3bf4f6286846a9741d67
Kemo Allen <kemo@claudia-allens-iMac-5.local>
2020-12-19 20:50:18 -0600

    New

---
+++ P0_KemoAllen/program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P0_KemoAllen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getTemperature();
+        }
+        public static void getTemperature()
+        {
+            bool temperatureParsed;
+            int num;
+
+            Console.WriteLine("Please enter a temperature between -40 and 130 degrees Fahrenheit.");
+            string response = Console.ReadLine();
+            temperatureParsed = int.TryParse(response, out num); 
+            Console.WriteLine("You said the temperature was " + num);
+
+            if(num <= 30){
+                Console.WriteLine("Really cold");
+            
+            }
+            else if(num <= 90){
+                Console.WriteLine("Warm");
+            }
+            else{
+                Console.WriteLine("Really hot");
+            }
+        }
+    }
+}
