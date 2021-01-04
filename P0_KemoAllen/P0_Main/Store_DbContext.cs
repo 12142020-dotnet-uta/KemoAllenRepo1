4fea25b52dd43103ec72dec9287b51b935dd56a7
Kemo Allen <kemo@claudia-allens-iMac-5.local>
2021-01-03 23:19:24 -0600

    2nd Jan3changes

--- P0_KemoAllen/P0_Main/Store_DbContext.cs
+++ P0_KemoAllen/P0_Main/Store_DbContext.cs
@@ -6,6 +6,11 @@ namespace P0_KemoAllen
     public class Store_DbContext : DbContext
     {
         //public DbSet<> of objects
+        public Store_DbContext()
+        {}
+        public Store_DbContext(DbContextOptions<Store_DbContext> options) : base(options)
+        {}
+        
         public DbSet<Customer> customers {get; set;}
         public DbSet<Location> locations {get; set;}
         public DbSet<Order> orders {get; set;}
@@ -14,8 +19,11 @@ namespace P0_KemoAllen
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            if(!optionsBuilder.IsConfigured)
+            {
             optionsBuilder.UseSqlServer("Server=tcp:kemoazserver.database.windows.net,1433;Initial Catalog=p0_mysql;Persist Security Info=False;"
             + "User ID=kemoallen;Password=Sholos_03;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
         }
         
     }
